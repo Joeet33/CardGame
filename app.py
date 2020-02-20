01c923adb4b714366e04416b4340bbe45cab3dd8
Joeet33 <55982242+Joeet33@users.noreply.github.com>
2020-02-20 11:25:07 +0000

    Add files via upload
    Completed basic blackjack

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import random
 import os
-# hand total
-def calc_hand(hand):
-    sum = 0
+import random
 
-#
-    non_aces = [deck for deck in hand if deck != 'A']
-    aces = [deck for deck in hand if deck != 'A']
+def calc_hand(hand):
+    non_aces = [c for c in hand if c != 'A']
+    aces = [c for c in hand if c == 'A']
+
+    sum = 0
 
     for deck in non_aces:
         if deck in 'JQK':
@@ -22,31 +21,70 @@ def calc_hand(hand):
 
     return sum
 
-# deck
-deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', ]*4
-
-random.shuffle(deck)
-
-# dealer
-dealer = []
-
-# player
-player = []
-
-# draw a card
-player.append(deck.pop())
-dealer.append(deck.pop())
-player.append(deck.pop())
-dealer.append(deck.pop())
-
-# clears screen
 while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
+    deck = [
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',]*4
 
-    player_score = calc_hand(player)
-    dealer_score = calc_hand(dealer)
+    random.shuffle(deck)
 
-    print('Dealer Cards: [{}][?]'.format(dealer[0]))
-    print('Your Cards [{}] ({})'.format(']['.join(player), player_score))
+    dealer = []
+    player = []
 
-    break
+    player.append(deck.pop())
+    dealer.append(deck.pop())
+    player.append(deck.pop())
+    dealer.append(deck.pop())
+
+    first_hand = True
+    sticking = False
+
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        player_score = calc_hand(player)
+        dealer_score = calc_hand(dealer)
+
+        if sticking:
+            print('Dealer Cards: [{}] ({})'.format(']['.join(dealer), dealer_score))
+        else:
+            print('Dealer Cards: [{}][?]'.format(dealer[0]))
+
+        print('Your Cards:   [{}] ({})'.format(']['.join(player), player_score))
+        print('')
+
+        if sticking:
+            if dealer_score > 21:
+                print('Dealer busted, you win!')
+            elif player_score == dealer_score:
+                print('Draw, no winner!')
+            elif player_score > dealer_score:
+                print('You beat the dealer, you win!')
+            else:
+                print('You lose\n')
+            input('Play again? Hit enter to continue')
+            break
+
+        if first_hand and player_score == 21:
+            print('Blackjack!\n')
+            input('Play again? Hit enter to continue')
+            break
+
+        if player_score > 21:
+            print('You bust!\n')
+            input('Play again? Hit enter to continue')
+            break
+
+        print('What would you like to do?')
+        print(' [1] Stick')
+        print(' [2] Twist\n')
+        choice = input('Your choice: ')
+
+
+        first_hand = False
+
+        if choice == '1':
+            sticking = True
+            while calc_hand(dealer) <= 16:
+                dealer.append(deck.pop())
+        elif choice == '2':
+            player.append(deck.pop())
