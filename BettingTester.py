a1918046ceea9045a30d47ad893572845b22af36
Joeet33 <55982242+Joeet33@users.noreply.github.com>
2020-02-24 18:06:53 +0000

    labelled what i have to do

--- BettingTester.py
+++ BettingTester.py
@@ -1,10 +1,16 @@
+# Amount of chips player has
 player1_chips = ['1-1000']
 
-
-
+# Amount of chips player wants to bet
 amount_of_chips = input('Place your bet: ')
 
 while amount_of_chips > '1000':
     if amount_of_chips > '1000':
         print("You don't have enough funds")
         break
+
+# A deduction from players total chips of the total bet
+
+# If win double that money
+
+# Else total stays at what was deducted
