4a3be393f51215aa501b69252277590b24be8735
Joeet33 <55982242+Joeet33@users.noreply.github.com>
2020-02-24 17:51:29 +0000

    Testing betting option

---
+++ BettingTester.py
@@ -0,0 +1,10 @@
+player1_chips = ['1-1000']
+
+
+
+amount_of_chips = input('Place your bet: ')
+
+while amount_of_chips > '1000':
+    if amount_of_chips > '1000':
+        print("You don't have enough funds")
+        break
