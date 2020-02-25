15fbbea6ed5ffb55e7e74b1d8a5795bb7407a3cf
Joeet33 <55982242+Joeet33@users.noreply.github.com>
2020-02-25 21:32:31 +0000

    implemented simplified betting
    Need to make it work through multiple rounds

--- app.py
+++ app.py
@@ -2,6 +2,40 @@ import os
 import random
 
 
+# Amount of chips player has
+amount_of_chips = 10000
+
+
+print('Player 1 has {}\n'.format(amount_of_chips))
+
+
+# Amount of chips player wants to bet
+while True:
+    try:
+        placed_bet = int(input('Place your bet: '))
+    except ValueError:
+        print("Sorry, I didn't understand that.")
+        continue
+    print('')
+
+    if placed_bet > 10000:
+        print("You don't have enough funds")
+        continue
+    else:
+        break
+
+# A deduction from players total chips of the total bet
+total_chips = (amount_of_chips - placed_bet)
+
+print('You now have: {}\n'.format(total_chips))
+
+# If player wins 'placed_bet' will double + original 'placed_bet' will be added back to 'amount_of_chips'. Giving a new 'total_chips'
+
+winnings = (placed_bet * 2)
+winning_total = (winnings + placed_bet)
+
+losing_total = (amount_of_chips - placed_bet)
+
 # Sorting out JQK + A's
 def calc_hand(hand):
     non_aces = [c for c in hand if c != 'A']
@@ -28,14 +62,14 @@ def calc_hand(hand):
 while True:
     deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', ] * 4
 
-    # Randomises/shuffling cards
+# Randomises/shuffling cards
     random.shuffle(deck)
 
-    # Player 1 + computer
+# Player 1 + computer
     dealer = []
     player = []
 
-    # Dealing cards
+# Dealing cards
     player.append(deck.pop())
     dealer.append(deck.pop())
     player.append(deck.pop())
@@ -50,42 +84,48 @@ while True:
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
 
-        # Value of player + computers hands
+# Value of player + computers hands
         player_score = calc_hand(player)
         dealer_score = calc_hand(dealer)
 
         if sticking:
             print('Dealer Cards: [{}] ({})'.format(']['.join(dealer), dealer_score))
-        # Hiding dealers second card
+# Hiding dealers second card
         else:
             print('Dealer Cards: [{}][?]'.format(dealer[0]))
-        # Showing both players cards
+# Showing both players cards
         print('Your Cards:   [{}] ({})\n'.format(']['.join(player), player_score))
 
-        # Game outcomes
+# Game outcomes
         if sticking:
             if dealer_score > 21:
-                print('Dealer busted, you win!')
+                print('Dealer busted, you win!\n')
+                print('You now have: {}'.format(winning_total))
             elif player_score == dealer_score:
                 print('Draw, no winner!')
+                print('You now have: {}'.format(losing_total))
             elif player_score > dealer_score:
                 print('You beat the dealer, you win!')
+                print('You now have: {}'.format(winning_total))
             else:
                 print('You lose\n')
+                print('You now have: {}\n'.format(losing_total))
             input('Play again? Hit enter to continue')
             break
 
         if first_hand and player_score == 21:
             print('Blackjack!\n')
+            print('You now have: {}\n'.format(winning_total))
             input('Play again? Hit enter to continue')
             break
 
         if player_score > 21:
             print('You bust!\n')
+            print('You now have: {}\n'.format(losing_total))
             input('Play again? Hit enter to continue')
             break
 
-        # Option to stick or twist
+# Option to stick or twist
         print('What would you like to do?')
         print(' [1] Stick')
         print(' [2] Twist\n')
