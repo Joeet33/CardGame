dd527abca34f4695e0c0b08ff13ff08d48205f02
Joeet33 <55982242+Joeet33@users.noreply.github.com>
2020-02-25 18:59:54 +0000

    Basic amount bet

--- BettingTester.py
+++ BettingTester.py
@@ -1,17 +1,25 @@
 # Amount of chips player has
-player1_chips = ['1-1000']
+available_to_bet = list(range(1, 1001))
 
-chips = 1000
+# Need to change so that you can lose + gain chips
+amount_of_chips = 1000
+
+
+print('Player 1 has {}\n'.format(amount_of_chips))
 
 
 # Amount of chips player wants to bet
-amount_of_chips = input('Place your bet: ')
+placed_bet = int(input('Place your bet: '))
 
-if amount_of_chips > '1000':
+print('')
+
+if placed_bet > 1000:
     print("You don't have enough funds")
-
+elif placed_bet < 1000:
+    print('You have bet {}'.format(placed_bet))
 
 # A deduction from players total chips of the total bet
+print('You now have: {}\n'.format(amount_of_chips - placed_bet))
 
 # If win double that money
 
