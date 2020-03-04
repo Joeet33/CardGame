3ffcf9356e633d26c1dcbf8118fdaf6b2621f7d4
Joeet33 <55982242+Joeet33@users.noreply.github.com>
2020-03-04 14:42:27 +0000

    Fixed
    Issue where it asked you to bet after every twist.

--- app.py
+++ app.py
@@ -50,9 +50,9 @@ while True:
         os.system('cls' if os.name == 'nt' else 'clear')
 
         # Amount of chips player has
-        amount_of_chips = 1000
+        starting_chips = 1000
 
-        print('Player 1 has {}\n'.format(amount_of_chips))
+        print('Player 1 has {}\n'.format(starting_chips))
 
         # Amount of chips player wants to bet
         while True:
@@ -70,74 +70,74 @@ while True:
                 break
 
         # A deduction from players total chips of the total bet
-        total_chips = (amount_of_chips - placed_bet)
+        total_chips = (starting_chips - placed_bet)
 
-        losing_total = (amount_of_chips - placed_bet)
+        losing_total = (starting_chips - placed_bet)
 
         print('You now have: {}\n'.format(total_chips))
 
         # If player wins 'placed_bet' will double + original 'placed_bet' will be added back to 'amount_of_chips'. Giving a new 'total_chips'
 
-        total_chips = ((placed_bet * 2) + amount_of_chips)
+        total_chips = ((placed_bet * 2) + starting_chips)
+        while True:
+        # Value of player + computers hands
+                player_score = calc_hand(player)
+                dealer_score = calc_hand(dealer)
 
-# Value of player + computers hands
-        player_score = calc_hand(player)
-        dealer_score = calc_hand(dealer)
+                if sticking:
+                    print('Dealer Cards: [{}] ({})'.format(']['.join(dealer), dealer_score))
+        # Hiding dealers second card
+                else:
+                    print('Dealer Cards: [{}][?]'.format(dealer[0]))
+        # Showing both players cards
+                print('Your Cards:   [{}] ({})\n'.format(']['.join(player), player_score))
 
-        if sticking:
-            print('Dealer Cards: [{}] ({})'.format(']['.join(dealer), dealer_score))
-# Hiding dealers second card
-        else:
-            print('Dealer Cards: [{}][?]'.format(dealer[0]))
-# Showing both players cards
-        print('Your Cards:   [{}] ({})\n'.format(']['.join(player), player_score))
+        # Game outcomes
+                if sticking:
+                    if dealer_score > 21:
+                        print('Dealer busted, you win!\n')
+                        print('You now have: {}'.format(total_chips))
+                    elif player_score == dealer_score:
+                        print('Draw, no winner!')
+                        print('You now have: {}'.format(losing_total))
+                    elif player_score > dealer_score:
+                        print('You beat the dealer, you win!')
+                        print('You now have: {}'.format(total_chips))
+                    else:
+                        print('You lose\n')
+                        print('You now have: {}\n'.format(losing_total))
+                    input('Play again? Hit enter to continue')
+                    break
 
-# Game outcomes
-        if sticking:
-            if dealer_score > 21:
-                print('Dealer busted, you win!\n')
-                print('You now have: {}'.format(total_chips))
-            elif player_score == dealer_score:
-                print('Draw, no winner!')
-                print('You now have: {}'.format(losing_total))
-            elif player_score > dealer_score:
-                print('You beat the dealer, you win!')
-                print('You now have: {}'.format(total_chips))
-            else:
-                print('You lose\n')
-                print('You now have: {}\n'.format(losing_total))
-            input('Play again? Hit enter to continue')
-            break
+                if first_hand and player_score == 21:
+                    print('Blackjack!\n')
+                    print('You now have: {}\n'.format(total_chips))
+                    input('Play again? Hit enter to continue')
+                    break
 
-        if first_hand and player_score == 21:
-            print('Blackjack!\n')
-            print('You now have: {}\n'.format(total_chips))
-            input('Play again? Hit enter to continue')
-            break
+                if player_score > 21:
+                    print('You bust!\n')
+                    print('You now have: {}\n'.format(losing_total))
+                    input('Play again? Hit enter to continue')
+                    break
 
-        if player_score > 21:
-            print('You bust!\n')
-            print('You now have: {}\n'.format(losing_total))
-            input('Play again? Hit enter to continue')
-            break
+        # Option to stick or twist
+                print('What would you like to do?')
+                print(' [1] Stick')
+                print(' [2] Twist\n')
+                choice = input('Your choice: ')
 
-# Option to stick or twist
-        print('What would you like to do?')
-        print(' [1] Stick')
-        print(' [2] Twist\n')
-        choice = input('Your choice: ')
+        # Showing it's not players first hand
+                first_hand = False
 
-# Showing it's not players first hand
-        first_hand = False
+        # Sticking if input = 1
+                if choice == '1':
+                    sticking = True
 
-# Sticking if input = 1
-        if choice == '1':
-            sticking = True
+        # Keep going until dealer gets <= 15
+                    while calc_hand(dealer) <= 15:
+                        dealer.append(deck.pop())
 
-# Keep going until dealer gets <= 15
-            while calc_hand(dealer) <= 15:
-                dealer.append(deck.pop())
-
-# Or twist again if input = 2
-        elif choice == '2':
-            player.append(deck.pop())
+        # Or twist again if input = 2
+                elif choice == '2':
+                    player.append(deck.pop())
